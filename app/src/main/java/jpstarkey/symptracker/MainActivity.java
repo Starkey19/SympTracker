4b917661d13e60a8d8d68a4898688f7b4176b4af
Starkey19 <josh.starkey19@gmail.com>
2017-03-23 17:12:48 +0000

    Added navigation bar and one template fragment for graphing, added icons to nav bar. Started preferences page

--- app/src/main/java/jpstarkey/symptracker/MainActivity.java
+++ app/src/main/java/jpstarkey/symptracker/MainActivity.java
@@ -1,15 +1,146 @@
 package jpstarkey.symptracker;
 
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.support.design.internal.NavigationMenu;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
+        implements daily.OnFragmentInteractionListener
 {
+
+    //Navigation drawer
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Replaces the actionbar with my toolbar layout:
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Finds the layout for the navigation drawer:
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //Set the toggle for open/close of navbar
+        drawerToggle = setupDrawerToggle();
+        mDrawer.addDrawerListener(drawerToggle);
+        //Setup the drawer view:
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        setupDrawerContent(nvDrawer);
     }
+
+
+    //region Hamburger icon animations
+    private ActionBarDrawerToggle setupDrawerToggle()
+    {
+        //Make sure you pass in a valid toolbar reference
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        //Sync the toggle state
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        //Pass config change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (drawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //endregion
+
+    //region navigation drawer business logic
+    private void setupDrawerContent(NavigationView navigationView)
+    {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener()
+                {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem)
+                    {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                }
+        );
+    }
+
+    //Business logic to determine which fragment to show after clicking an item on the
+    //navigation drawer.
+    public void selectDrawerItem(MenuItem menuItem)
+    {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = daily.class;//FirstFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = daily.class;//SecondFragment.class;
+                break;
+            case R.id.nav_third_fragment:
+                fragmentClass = daily.class;//ThirdFragment.class;
+                break;
+            default:
+                fragmentClass = daily.class;//FirstFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
+    //endregion
+
+    //For fragment interaction:
+    @Override
+    public void onFragmentInteraction(Uri uri)
+    {
+
+    }
+
+
 }
